0418cbd5205fd70b84fc49e9c2ac357928f242f2
Lenin Compres <lenincompres@gmail.com>
2024-10-24 19:50:53 -0400

    pets gallery finished

--- Class 08/petsGallery/main.js
+++ Class 08/petsGallery/main.js
@@ -3,15 +3,54 @@ addButton.onclick = function(){
   addNewPet();
 };
 
+
+function validateInput(inputElt){
+  if(inputElt.value.trim().length === 0){
+    inputElt.classList.add("error");
+    return false;
+  } else {
+    inputElt.classList.remove("error");
+    return true;
+  }
+}
+
+
+function clearForm(){
+  nameInput.value = '';
+  ageInput.value = '';
+  pictureInput.value = '';
+}
+
+
 function addNewPet(){
 
-  petsGalleryMain.innerHTML += `
-    <section>
-      <h2>${nameInput.value}</h2>
-      <img src="${pictureInput.value}" alt="${nameInput.value}'s picture"/>
-      <p>${nameInput.value} is ${ageInput.value} years old</p>
-      <button>Remove</button>
-    </section>
+  if(!validateInput(nameInput)){
+    return;
+  }
+  if(!validateInput(ageInput)){
+    return;
+  }
+  if(!validateInput(pictureInput)){
+    return;
+  }
+
+  let petSection = document.createElement('section');
+
+  petSection.innerHTML += `
+    <h2>${nameInput.value}</h2>
+    <img src="${pictureInput.value}" alt="${nameInput.value}'s picture"/>
+    <p>${nameInput.value} is ${ageInput.value} years old</p>
   `;
+
+  let removeButton = document.createElement('button');
+  removeButton.innerText = "Remove";
+  removeButton.onclick = function(){
+    petSection.remove();
+  }
+  petSection.append(removeButton);
+
+  petsGalleryMain.append(petSection);
+
+  clearForm();
 
 }
