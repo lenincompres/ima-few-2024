be0488f87f8d3642061977c203a4d6b161dd571a
Lenin Compres <lenincompres@gmail.com>
2024-11-08 11:19:48 -0500

    API samples

--- Week 10/catBreeds/main.js
+++ Week 10/catBreeds/main.js
@@ -1,7 +1,21 @@
 
+/**
+ * The fecth method allows us to load filed from any url.
+ * Since this may take a while, we need to give it a function for it to know what to to with the information of the file received, once it is received.
+ * This is done in a then call (a promise) tha handles the response. 
+ */
+
 fetch("cats.json").then(response => {
+  /**
+   * A HTTP request returns a response, which is like an evelope with all the information about the transfer. 
+   * We then need to tell it what to do with the content.
+   * Here I use the json method of the response object so that the info is turn into a javascript object—instead of a text file, blob (image), etc.
+   * This process may alro take a while, so it returns a promise as well.
+   */
   return response.json();
+
 }).then(data => {
+  // This is the json object in a variable called data.
   console.log(data);
   mainContent.innerHTML = `<h2>${data.description}</h2>`;
   let selectElement = document.createElement('select');
@@ -16,22 +30,31 @@ fetch("cats.json").then(response => {
   selectElement.onchange = () => {
     wikipediaFrame.src = "https://en.wikipedia.org/wiki/" + selectElement.value + " cat";
   };
+
 }).catch(error => {
+  /**
+   * the catch promise, tellf fetch what to do in cases there's an error or issue with the request of with the response. This is called error handling.
+   */
   console.error(error);
+
 });
 
+
+/**
+ * This code is executed after the fecth method is initiated, but does not wait fromt the response to arrive. That will happen "asychronously".
+ */
 document.body.style.backgroundColor = "lime";
 
 
 /**
- * Grabbing an image with fetch
+ * Example of frabbing an image with fetch.
  */
-
 const picURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/British_burmese_-_Andel_Alois_at_Cat_show.JPG/220px-British_burmese_-_Andel_Alois_at_Cat_show.JPG";
 
 fetch(picURL).then(response => {
   return response.blob();
 }).then(blob => {
+  // To use a image blob as a URL to link to an img or style, you need to turn thsi blob into a url with the following method.
   const url = URL.createObjectURL(blob);
   document.body.style.backgroundImage = `url(${url})`; 
 });
--- Week 10/peopleInSpace/main.js
+++ Week 10/peopleInSpace/main.js
@@ -1,28 +1,40 @@
 
+/**
+ * With the DOM.js library linked on the index page we get a "syntactic sugar".
+ * It allows us to write HTML and CSS programatically in JS object notation.
+ * The use of the DOM.set method, adds elements and properties to the DOM—inside the document.body.
+ */
 
 DOM.set({
   width: "40em",
   margin: "5em auto",
   h1: "People in space",
   main: {
-    h2: "THis is how mane people are in space right now",
+    h2: "This is how many people are in space right now.",
     section: {
       id: "listElement",
       display: "flex",
       flexWrap: "wrap",
       margin: "1em",
-      height: "20em",
       backgroundColor: "wheat",
       innerText: "some text",
     },
   },
 });
 
-
+/**
+ * The use of async and await, also provide a type of syntactic sugar.
+ * It allows us to use fetch (and other promise methods) in a line of code—
+ * simulating a pause or wait in the code execution.
+ * await can only be used inside an asychronous function.
+ */
 async function loadPeople(){
   let response = await fetch("http://api.open-notify.org/astros.json");
   let json = await response.json();
   for(let person of json.people){
+    /**
+     * The set method from the DOM.js' library is available for all HTTML elements.
+     */
     listElement.set({
       section:{ 
         margin: "1em",
@@ -34,5 +46,6 @@ async function loadPeople(){
     });
   }
 };
+
 loadPeople();
 
